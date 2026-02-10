7f82dafef2bc627fbd84c7f8eeeaa72f2bb6179a
smallcracker <hhh>
2026-02-10 08:58:21 +0000

    format only

--- source/isaaclab_tasks/isaaclab_tasks/manager_based/manipulation/stack/config/franka/stack_ik_rel_visumotor_rewarded_env_cfg.py
+++ source/isaaclab_tasks/isaaclab_tasks/manager_based/manipulation/stack/config/franka/stack_ik_rel_visumotor_rewarded_env_cfg.py
@@ -6,15 +6,7 @@
 import math
 
 import isaaclab.sim as sim_utils
-from isaaclab.assets import ArticulationCfg, AssetBaseCfg
-from isaaclab.envs import ManagerBasedRLEnvCfg
-from isaaclab.managers import EventTermCfg as EventTerm
-from isaaclab.managers import ObservationGroupCfg as ObsGroup
-from isaaclab.managers import ObservationTermCfg as ObsTerm
 from isaaclab.managers import RewardTermCfg as RewTerm
-from isaaclab.managers import SceneEntityCfg
-from isaaclab.managers import TerminationTermCfg as DoneTerm
-from isaaclab.scene import InteractiveSceneCfg
 from isaaclab.utils import configclass
 from isaaclab.sensors import TiledCameraCfg
 
@@ -23,22 +15,11 @@ from isaaclab.sensors import TiledCameraCfg
 # Pre-defined configs
 ##
 
-from isaaclab_assets.robots.cartpole import CARTPOLE_CFG  # isort:skip
 from isaaclab_tasks.manager_based.manipulation.stack.config.franka.stack_ik_rel_visuomotor_env_cfg import (
     FrankaCubeStackVisuomotorEnvCfg,
 )  # isort:skip
 from isaaclab_tasks.manager_based.manipulation.stack import mdp  # isort:skip
 
-from isaaclab.devices.device_base import DeviceBase, DevicesCfg
-from isaaclab.devices.keyboard import Se3KeyboardCfg
-from isaaclab.devices.gamepad import Se3GamepadCfg
-from isaaclab.devices.openxr.openxr_device import OpenXRDeviceCfg
-from isaaclab.devices.openxr.retargeters.manipulator.gripper_retargeter import (
-    GripperRetargeterCfg,
-)
-from isaaclab.devices.openxr.retargeters.manipulator.se3_rel_retargeter import (
-    Se3RelRetargeterCfg,
-)
 
 ##
 # Scene definition
@@ -59,11 +40,13 @@ class RewardsCfg:
 
 @configclass
 class FrankaCubeStackVisuomotorRewardedEnvCfg(FrankaCubeStackVisuomotorEnvCfg):
-    rewards: RewardsCfg = RewardsCfg()  #add reward terms
+    rewards: RewardsCfg = RewardsCfg()  # add reward terms
 
     def __post_init__(self):
         super().__post_init__()
-        self.sim.render_interval = 5   # set render interval as the same as the decimation interval
+        self.sim.render_interval = (
+            5  # set render interval as the same as the decimation interval
+        )
 
         # Set cameras
         # Set wrist camera
@@ -74,10 +57,15 @@ class FrankaCubeStackVisuomotorRewardedEnvCfg(FrankaCubeStackVisuomotorEnvCfg):
             width=200,
             data_types=["rgb", "distance_to_image_plane"],
             spawn=sim_utils.PinholeCameraCfg(
-                focal_length=24.0, focus_distance=400.0, horizontal_aperture=20.955, clipping_range=(0.1, 2)
+                focal_length=24.0,
+                focus_distance=400.0,
+                horizontal_aperture=20.955,
+                clipping_range=(0.1, 2),
             ),
             offset=TiledCameraCfg.OffsetCfg(
-                pos=(0.13, 0.0, -0.15), rot=(-0.70614, 0.03701, 0.03701, -0.70614), convention="ros"
+                pos=(0.13, 0.0, -0.15),
+                rot=(-0.70614, 0.03701, 0.03701, -0.70614),
+                convention="ros",
             ),
         )
 
@@ -89,10 +77,14 @@ class FrankaCubeStackVisuomotorRewardedEnvCfg(FrankaCubeStackVisuomotorEnvCfg):
             width=200,
             data_types=["rgb", "distance_to_image_plane"],
             spawn=sim_utils.PinholeCameraCfg(
-                focal_length=24.0, focus_distance=400.0, horizontal_aperture=20.955, clipping_range=(0.1, 2)
+                focal_length=24.0,
+                focus_distance=400.0,
+                horizontal_aperture=20.955,
+                clipping_range=(0.1, 2),
             ),
             offset=TiledCameraCfg.OffsetCfg(
-
-                pos=(1.0, 0.0, 0.4), rot=(0.35355, -0.61237, -0.61237, 0.35355), convention="ros"
+                pos=(1.0, 0.0, 0.4),
+                rot=(0.35355, -0.61237, -0.61237, 0.35355),
+                convention="ros",
             ),
         )
